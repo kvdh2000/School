b2d4d96facffefa713baad22422ad95d1ad1a0a2
kvdh2000 <kishanvdh@gmail.com>
2018-12-05 11:25:05 +0100

    Alg 1 en cc 1

--- Algoritmiek/Algoritmiek/Algoritmiek/1.cpp
+++ Algoritmiek/Algoritmiek/Algoritmiek/1.cpp
@@ -1,7 +1,53 @@
 
 #include <iostream>
+#include <algorithm>
+#include <array>
+#include <stdio.h> 
+#include <cstdlib>
+#include <chrono>
 
+using namespace std ::chrono;
+
+// Driver program to test above functions 
 int main()
 {
+	const int length = 10000;
+	int arr[length];
+	char yn;
+	int count = 0;
+	int times[10];
+
+	do
+	{
+		for (int i = 0; i < length; i++)
+		{
+			arr[i] = rand();
+			//std::cout << arr[i] << std::endl;
+		}
+
+		auto start = high_resolution_clock::now();
+
+		size_t size = sizeof(arr) / sizeof(arr[0]);
+		std::sort(arr, arr + size);
+		for (size_t i = 0; i < size; ++i) {
+			std::cout << arr[i] << ' ';
+		}
+
+		auto stop = high_resolution_clock::now();
+
+		auto duration = duration_cast<microseconds>(stop - start);
+
+		times[count] = duration.count();
+
+		std::cout << std::endl << std::endl << duration.count() * 0.000001 << "  seconds" << std::endl << std::endl;
+
+		count++;
+	} while (count < 10);
+	
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << times[i] * 0.000001 << std::endl;
+	}
+
 	return 0;
 }
--- CCP/CCAsssignments/CCAsssignments/1.cpp
+++ CCP/CCAsssignments/CCAsssignments/1.cpp
@@ -1,11 +1,7 @@
-
 #include <fstream>
+#include <complex> // if you make use of complex number facilities in C++
 #include <iostream>
-#include <complex>
-
 using namespace std;
-
-int hw = 1600;
 
 template <class T> struct RGB { T r, g, b; };
 
@@ -55,42 +51,76 @@ public:
 		out << "P6" << std::endl << _cols << " " << _rows << std::endl << 255 << std::endl;
 		for (size_t y = 0; y < _rows; y++)
 			for (size_t x = 0; x < _cols; x++)
-				out << value(x, y) << value(x, y) << value(x, y);
-	}
-
-	int value(int x, int y) {
-		complex<float> point((float)x / hw - 1.5, (float)y / hw - 0.5);
-		complex<float> z(0, 0);
-		unsigned int nb_iter = 0;
-		while (abs(z) < 2 && nb_iter <= 34) {
-			z = z * z + point;
-			nb_iter++;
-		}
-		if (nb_iter < 10) return 255;
-		else return 0;
+				out << _matrix[y][x].r << _matrix[y][x].g << _matrix[y][x].b;
 	}
 };
+int findMandelbrot(double cr, double ci, int max_iterations)
+{
+	int i = 0;
+	double zr = 0.0, zi = 0.0;
+	while (i < max_iterations && zr * zr + zi * zi < 4.0)
+	{
+		double temp = zr * zr - zi * zi + cr;
+		zi = 2.0 * zr * zi + ci;
+		zr = temp;
+		i++;
+	}
 
+	return i;
+}
+
+double mapToReal(int x, int imageWidth, double minR, double maxR)
+{
+	double range = maxR - minR;
+	return x * (range / imageWidth) + minR;
+}
+
+double mapToImaginary(int y, int imageHeight, double minI, double maxI)
+{
+	double range = maxI - minI;
+	return y * (range / imageHeight) + minI;
+}
 
 int main()
 {
-	const unsigned width = hw;
-	const unsigned height = hw;
-
+	const unsigned width = 512;
+	const unsigned height = 512;
+	int maxN = 255;
+	double minR = -1.5, maxR = 0.7, minI = -1, maxI = 1;
 	PPMImage image(height, width);
 
-	/*
-	image[y][x].r = image[y][x].g = image[y][x].b = 255; // white pixel
+	// Open the output file, write the PPM header...
+	ofstream fout("output_image.ppm");
+	fout << "P3" << endl; // "Magic Number" - PPM file
+	fout << width << " " << height << endl; // Dimensions
+	fout << "255" << endl; // Maximum value of a pixel R,G,B value...
 
-	image[y][x].r = image[y][x].g = image[y][x][b] = 0; // black pixel
+	// For every pixel...
+	for (int y = 0; y < height; y++) // Rows...
+	{
+		for (int x = 0; x < width; x++) // Pixels in row (columns)...
+		{
+			// ... Find the real and imaginary values for c, corresponding to that
+			//     x, y pixel in the image.
+			double cr = mapToReal(x, width, minR, maxR);
+			double ci = mapToImaginary(y, height, minI, maxI);
 
-	// red pixel
-	image[y][x].r = 255;
-	image[y][x].g = 0;
-	image[y][x].b = 0;
-	*/
-		
+			// ... Find the number of iterations in the Mandelbrot formula
+			//     using said c.
+			int n = findMandelbrot(cr, ci, maxN);
+
+			// ... Map the resulting number to an RGP value
+			int r = (n % 256);
+			int g = (n % 256);
+			int b = (n % 256);
+
+			// ... Output it to an image
+			fout << r << " " << g << " " << b << " ";
+		}
+		fout << endl;
+	}
+	fout.close();
+
 	image.save("mandelbrot.ppm");
 	return 0;
-	
 }
