c9088f670de66c52eab3cb0bf88e52b3a9502415
kvdh2000 <kishanvdh@gmail.com>
2019-04-03 10:27:57 +0200

    Finished analytics

--- P2.3/Analytics/opgaven_data/conf.R
+++ P2.3/Analytics/opgaven_data/conf.R
@@ -1,6 +1,27 @@
 conf <- function(n, d) 
 {
-  predicttest= predict (tree2,newdata = test)
+  predicttest= predict (tree, newdata=testData)
+  t=table(n, predicttest[,2] > d)
+  
+  TN=t[1]
+  FP=t[2]
+  FN=t[3]
+  TP=t[4]
+  
+  sensitivity <- TP / (FN + TP)
+  specificity <- TN / (TN + FP)
+  accuracy <- (TN + TP) / (TN + TP + FP + FN)
+  
+  print(t)
+  
+  print(paste("Sensitivity = ", sensitivity))
+  print(paste("Specificify = ", specificity))
+  print(paste("Accuracy =    ", accuracy))
+}
+
+conf3 <- function(n, d) 
+{
+  predicttest= predict (tree, newdata=testData)
   t=table(n, predicttest[,2] > d)
   
   TN=t[1]
